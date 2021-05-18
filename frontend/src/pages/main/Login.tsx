aad4b8424dff9c073cfe65007df5bd6749cea5c0
Freddy L <neo-ciber_94@hotmail.com>
2021-05-18 14:01:27 -0400

    Fixed bug onsubmit in login form

--- frontend/src/pages/main/Login.tsx
+++ frontend/src/pages/main/Login.tsx
@@ -1,18 +1,18 @@
-import { Field, FormikErrors, FormikProps, withFormik } from "formik";
+import { Field, FormikErrors, FormikProps, Form, Formik } from "formik";
 
 interface FormValues {
   email: string;
   password: string;
 }
 
-const InnerForm = (props: FormikProps<FormValues>) => {
-  const { touched, errors, isSubmitting, isValid } = props;
+function LoginForm(props: { props: FormikProps<FormValues> }) {
+  const { touched, errors, isSubmitting } = props.props;
 
   const emailError = errors.email && touched;
   const passwordError = errors.password && touched;
 
   return (
-    <form>
+    <Form>
       {/* Email */}
       <div className="mb-4">
         <label
@@ -26,7 +26,7 @@ const InnerForm = (props: FormikProps<FormValues>) => {
           name="email"
           autoComplete="current-username"
           className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
-            emailError && "border-red-600"
+            errors.email && touched && "border-red-600"
           }`}
         />
         {emailError && <p className="text-red-500 text-xs">{errors.email}</p>}
@@ -57,53 +57,51 @@ const InnerForm = (props: FormikProps<FormValues>) => {
       <div className="text-right">
         <button
           type="submit"
-          disabled={isSubmitting || !isValid}
+          disabled={isSubmitting}
           className="bg-red-600 hover:bg-red-800 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline shadow-sm
         w-full md:w-32"
         >
           Login
         </button>
       </div>
-    </form>
+    </Form>
   );
-};
-
-const LoginForm = withFormik<{}, FormValues>({
-  mapPropsToValues: () => {
-    return {
-      email: "",
-      password: "",
-    };
-  },
-
-  validate: (values: FormValues) => {
-    let errors: FormikErrors<FormValues> = {};
-
-    if (values.email.trim().length === 0) {
-      errors.email = "Email required";
-    }
-
-    if (values.password.length === 0) {
-      errors.password = "Password required";
-    }
-
-    return errors;
-  },
-
-  handleSubmit: ({ email, password }) => {
-    console.log(email, password);
-  },
-})(InnerForm);
+}
 
 export default function Login() {
+  const initialValues: FormValues = { email: "", password: "" };
   return (
-    <>
-      <h1>Login</h1>
-      <div className="flex flex-row justify-center">
-        <div className="w-full md:w-4/6 p-6">
-          <LoginForm />
-        </div>
-      </div>
-    </>
+    <Formik
+      initialValues={initialValues}
+      validateOnChange={false}
+      validateOnBlur={false}
+      validate={(values) => {
+        const errors: FormikErrors<FormValues> = {};
+        if (values.email?.trim().length === 0) {
+          errors.email = "Email is required";
+        }
+
+        if (values.password?.trim().length === 0) {
+          errors.password = "Password is required";
+        }
+
+        return errors;
+      }}
+      onSubmit={(values, actions) => {
+        console.log({ values, actions });
+        actions.setSubmitting(false);
+      }}
+    >
+      {(props) => (
+        <>
+          <h1>Login</h1>
+          <div className="flex flex-row justify-center">
+            <div className="w-full md:w-3/6 px-6 py-12">
+              <LoginForm props={props} />
+            </div>
+          </div>
+        </>
+      )}
+    </Formik>
   );
 }

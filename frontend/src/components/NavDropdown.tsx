e374a037be6fc291b2fbe3ccea8fa7b655427381
Freddy L <neo-ciber_94@hotmail.com>
2021-06-16 01:00:41 -0400

    Added clickable to dropdown

--- frontend/src/components/NavDropdown.tsx
+++ frontend/src/components/NavDropdown.tsx
@@ -1,21 +1,54 @@
+import { useState } from "react";
 import "./NavDropdown.css";
+
+type DropdownBehaviour = "hover" | "click";
 
 interface NavDropdownProps {
   name: string;
+  behaviour?: DropdownBehaviour;
 }
 
-export const NavDropdown: React.FC<NavDropdownProps> = ({ name, ...props }) => {
-  // const [open, setOpen] = useState(false);
+export const NavDropdown: React.FC<NavDropdownProps> = (props) => {
+  const { behaviour = "click" } = props;
 
-  // const toggleDropdown = useCallback(() => setOpen(!open), []);
+  if (behaviour === "hover") {
+    return <HoverableDropdown {...props} />;
+  } else {
+    return <ClickableDropdown {...props} />;
+  }
+};
+
+const HoverableDropdown: React.FC<NavDropdownProps> = (props) => {
+  return (
+    <div className="hoverable dropdown cursor-pointer">
+      <div className="nav-link dropdown-btn select-none">
+        {props.name}
+        <i className="fa fa-caret-down ml-2"></i>
+      </div>
+      <div className="hoverable dropdown-content bg-gray-100 lg:bg-white shadow-inner lg:shadow">
+        {props.children}
+      </div>
+    </div>
+  );
+};
+
+const ClickableDropdown: React.FC<NavDropdownProps> = (props) => {
+  const [isOpen, setOpen] = useState(false);
 
   return (
     <div className="dropdown cursor-pointer">
-      <div className="nav-link dropdown-btn select-none">
-        {name}
+      <button
+        className="nav-link dropdown-btn select-none"
+        onClick={() => setOpen(!isOpen)}
+      >
+        {props.name}
         <i className="fa fa-caret-down ml-2"></i>
-      </div>
-      <div className="dropdown-content bg-gray-100 lg:bg-white shadow-inner lg:shadow">
+      </button>
+      <div
+        className={`dropdown-content bg-gray-100 lg:bg-white shadow-inner lg:shadow ${
+          isOpen ? "flex flex-col" : "hidden"
+        }`}
+      >
         {props.children}
       </div>
     </div>

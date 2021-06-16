0890b6616fd14973ea2d9cb99865c155a7b6746c
Freddy L <neo-ciber_94@hotmail.com>
2021-06-15 20:38:56 -0400

    Changed profile to new Routes API

--- frontend/src/pages/profile/ProfileRoutes.tsx
+++ frontend/src/pages/profile/ProfileRoutes.tsx
@@ -1,5 +1,7 @@
 import { Redirect, Route, Switch, useRouteMatch } from "react-router-dom";
 import { Footer, Header } from "src/layout";
+import Routes from "src/routes/Routes";
+import NotFound from "../common/NotFound";
 import { ChangePassword } from "./ChangePassword";
 import { EditProfile } from "./EditProfile";
 import { Profile } from "./Profile";
@@ -15,9 +17,9 @@ export function ProfileRoutes() {
       <Header title="Profile" />
       <Switch>
         <Route exact path={match.url} component={Profile} />
-        <Route exact path={`${match.url}/edit`} component={EditProfile} />
-        <Route exact path={`${match.url}/changepassword`} component={ChangePassword} />
-        <Redirect from={match.url} to={match.url} />
+        <Route exact path={Routes.join(match.url, "edit")} component={EditProfile} />
+        <Route exact path={Routes.join(match.url, "changepassword")} component={ChangePassword} />
+        <Route path="*" component={NotFound}/>
       </Switch>
       <Footer />
     </>

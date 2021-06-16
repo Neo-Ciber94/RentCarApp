884e6c5f412f22848525ba3421b5f356cf70e7e4
Freddy L <neo-ciber_94@hotmail.com>
2021-06-16 01:23:19 -0400

    Added animation on clickable dropdown

--- frontend/src/components/NavDropdown.tsx
+++ frontend/src/components/NavDropdown.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./NavDropdown.css";
 
 type DropdownBehaviour = "hover" | "click";
@@ -35,19 +35,42 @@ const HoverableDropdown: React.FC<NavDropdownProps> = (props) => {
 const ClickableDropdown: React.FC<NavDropdownProps> = (props) => {
   const [isOpen, setOpen] = useState(false);
 
+  const divRef = useRef<HTMLDivElement>(null);
+
+  const handleSetOpen = () => {
+    const open = !isOpen;
+    const element = divRef.current!;
+
+    if (open) {
+      const height = Array.from(element.children).reduce((prev, cur) => {
+        const style = window.getComputedStyle(cur);
+
+        // prettier-ignore
+        const py = parseFloat(style.paddingBottom) + parseFloat(style.paddingTop);
+        const my = parseFloat(style.marginBottom) + parseFloat(style.marginTop);
+        return parseFloat(style.height) + prev + py + my - 16;
+      }, 0);
+
+      element.style.height = height + "px";
+    } else {
+      element.style.height = "0px";
+    }
+
+    setOpen(open);
+  };
+
   return (
     <div className="dropdown cursor-pointer">
       <button
         className="nav-link dropdown-btn select-none"
-        onClick={() => setOpen(!isOpen)}
+        onClick={handleSetOpen}
       >
         {props.name}
         <i className="fa fa-caret-down ml-2"></i>
       </button>
       <div
-        className={`dropdown-content bg-gray-100 lg:bg-white shadow-inner lg:shadow ${
-          isOpen ? "flex flex-col" : "hidden"
-        }`}
+        ref={divRef}
+        className={`dropdown-content bg-gray-100 lg:bg-white shadow-inner lg:shadow flex flex-col overflow-hidden transition-all duration-300`}
       >
         {props.children}
       </div>
